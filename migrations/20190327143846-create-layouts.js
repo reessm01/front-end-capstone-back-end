109c3f4f72d8aa89a7f32cfa94803141c69b31b6
Scott Reese <scott_reese@pm.me>
2019-03-28 14:50:38 -0400

    changes made to make testing work

--- migrations/20190327143846-create-layouts.js
+++ migrations/20190327143846-create-layouts.js
@@ -8,7 +8,10 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      viewModel: {
+      userId: { 
+        type: Sequelize.INTEGER 
+      },
+      layout: {
         type: Sequelize.JSON
       },
       createdAt: {
